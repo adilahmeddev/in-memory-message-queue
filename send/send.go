2889ac1e669b4bcd5370322c72928ffb54dfed26
adilahmeddev <adilahmed97swe@gmail.com>
2022-05-12 16:57:22 +0100

    rabbit mq tutorial one

---
+++ send/send.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "failed to connect to rabbitmq")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil)
+	failOnError(err, "failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
