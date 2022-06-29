15eef546d4bd1b8284782a1dcf89d1f38197c907
adilahmeddev <adil.ahmed@saltpay.co>
2022-06-29 10:52:40 +0100

    fresh start, initial test

---
+++ memory_queue_test.go
@@ -0,0 +1,18 @@
+package events_notification
+
+import "testing"
+
+func TestQueue(t *testing.T) {
+
+	t.Run("I am able to add messages to the queue", func(t *testing.T) {
+
+	})
+
+}
+
+func TestProducer(t *testing.T) {
+	//t.Run("when I create a message with my producer, I expect it to be sent to its destination", func(t *testing.T) {
+	//	messageQueue := NewMessageQueue()
+	//	producer := NewProducer(messageQueue)
+	//})
+}
